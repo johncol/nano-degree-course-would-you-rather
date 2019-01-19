d8106582e3c1038115b3d2eafef3c0d65921aa0f
John Cely <john.19col@gmail.com>
2019-01-18 19:55:37 -0500

    Update input component:  - Style selected text  - Add a way to save the input reference

--- src/components/shared/input-field/InputField.jsx
+++ src/components/shared/input-field/InputField.jsx
@@ -3,11 +3,20 @@ import React, { Component } from 'react';
 import './input-field.scss';
 
 class InputField extends Component {
+  getInputProps = () => {
+    const inputProps = { ...this.props };
+    delete inputProps.saveRef;
+    delete inputProps.id;
+    delete inputProps.label;
+    return inputProps;
+  };
+
   render() {
-    const { id, label } = this.props;
+    const { id, label, saveRef } = this.props;
+    const inputProps = this.getInputProps();
     return (
       <div className="input-field">
-        <input {...this.props} />
+        <input {...inputProps} ref={saveRef} />
         <label htmlFor={id}>{label}</label>
       </div>
     );
