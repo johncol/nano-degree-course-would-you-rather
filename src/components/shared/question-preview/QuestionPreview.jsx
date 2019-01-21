4ad5f197b28ef20e611734963500d32995270d8d
John Cely <john.19col@gmail.com>
2019-01-20 22:41:22 -0500

    Update look of question preview component

--- src/components/shared/question-preview/QuestionPreview.jsx
+++ src/components/shared/question-preview/QuestionPreview.jsx
@@ -2,16 +2,32 @@ import React from 'react';
 
 import './question-preview.scss';
 
+const QuestionText = ({ text }) => (
+  <div className="question-preview__text content--red">{text}</div>
+);
+
+const QuestionButton = () => (
+  <button className="question-preview__button content--grey">
+    <i class="material-icons">check</i>
+  </button>
+);
+
+const QuestionOption = ({ option, text }) => {
+  const optionClassName = 'question-preview__option--' + (option === 1 ? 'one' : 'two');
+  return (
+    <section className={'question-preview__option ' + optionClassName}>
+      <QuestionButton />
+      <QuestionText text={text} />
+    </section>
+  );
+};
+
 const QuestionPreview = props => {
   const { optionOne, optionTwo } = props.question;
   return (
     <article className="question-preview">
-      <section className="question-preview__option content--red">
-        {optionOne.text}
-      </section>
-      <section className="question-preview__option content--grey">
-        {optionTwo.text}
-      </section>
+      <QuestionOption option={1} text={optionOne.text} />
+      <QuestionOption option={2} text={optionTwo.text} />
     </article>
   );
 };
