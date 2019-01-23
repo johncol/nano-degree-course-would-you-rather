a411bf3629534f902ec4e1231c357fd08dd60791
John Cely <john.19col@gmail.com>
2019-01-23 15:45:17 -0500

    Remove unused actions and reducers for questions

--- src/state/actions/questions.js
+++ src/state/actions/questions.js
@@ -1,22 +1,10 @@
 import * as API from './../../api/_DATA';
 import { LoaderAction } from './loader';
 
-const FETCH_QUESTION = 'FETCH_QUESTION';
-const FETCH_QUESTIONS = 'FETCH_QUESTIONS';
 const FETCH_ALL_QUESTIONS = 'FETCH_ALL_QUESTIONS';
 const ADD_QUESTION = 'ADD_QUESTION';
 
 const QuestionActionCreator = {
-  fetchQuestion: question => ({
-    type: FETCH_QUESTION,
-    payload: question
-  }),
-
-  fetchQuestions: questions => ({
-    type: FETCH_QUESTIONS,
-    payload: questions
-  }),
-
   fetchAllQuestions: questions => ({
     type: FETCH_ALL_QUESTIONS,
     payload: questions
@@ -26,35 +14,6 @@ const QuestionActionCreator = {
     type: ADD_QUESTION,
     payload: question
   })
-};
-
-const fetchQuestion = id => dispatch => {
-  dispatch(LoaderAction.showLoader());
-  return API._getQuestion(id)
-    .then(response => {
-      const question = response.question;
-      dispatch(LoaderAction.hideLoader());
-      dispatch(QuestionActionCreator.fetchQuestion(question));
-    })
-    .catch(error => {
-      dispatch(LoaderAction.hideLoader());
-      console.warn('Error fetching question:', id, error);
-    });
-};
-
-const fetchQuestions = ids => dispatch => {
-  dispatch(LoaderAction.showLoader());
-  const apiRequests = ids.map(API._getQuestion);
-  return Promise.all(apiRequests)
-    .then(responses => {
-      const questions = responses.map(response => response.question);
-      dispatch(LoaderAction.hideLoader());
-      dispatch(QuestionActionCreator.fetchQuestions(questions));
-    })
-    .catch(error => {
-      dispatch(LoaderAction.hideLoader());
-      console.warn('Error fetching questions:', ids, error);
-    });
 };
 
 const fetchAllQuestions = () => dispatch => {
@@ -85,15 +44,11 @@ const addQuestion = questionInfo => dispatch => {
 };
 
 export const QuestionActionType = {
-  FETCH_QUESTION,
-  FETCH_QUESTIONS,
   FETCH_ALL_QUESTIONS,
   ADD_QUESTION
 };
 
 export const QuestionAction = {
-  fetchQuestion,
-  fetchQuestions,
   fetchAllQuestions,
   addQuestion
 };
--- src/state/reducers/questions.js
+++ src/state/reducers/questions.js
@@ -2,12 +2,6 @@ import { QuestionActionType } from '../actions/questions';
 
 const questionsReducer = (state = {}, action) => {
   switch (action.type) {
-    case QuestionActionType.FETCH_QUESTION:
-      return fetchQuestion(state, action);
-
-    case QuestionActionType.FETCH_QUESTIONS:
-      return fetchQuestions(state, action);
-
     case QuestionActionType.FETCH_ALL_QUESTIONS:
       return fetchAllQuestions(state, action);
 
@@ -17,23 +11,6 @@ const questionsReducer = (state = {}, action) => {
     default:
       return state;
   }
-};
-
-const fetchQuestion = (state, action) => {
-  const question = action.payload;
-  return {
-    ...state,
-    [question.id]: question
-  };
-};
-
-const fetchQuestions = (state, action) => {
-  const newState = { ...state };
-  const questions = action.payload;
-  questions.forEach(question => {
-    newState[question.id] = question;
-  });
-  return newState;
 };
 
 const fetchAllQuestions = (state, action) => {
