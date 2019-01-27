2fd6fcb5a368cd5d2203acafd4232fd7e8d29c0a
John <john.19col@gmail.com>
2019-01-27 16:16:16 -0500

    Question preview component simplified
    Question preview component cannot be used anymore to answer questions, it only links to the question view page

--- src/components/home/question-preview/QuestionPreview.jsx
+++ src/components/home/question-preview/QuestionPreview.jsx
@@ -1,72 +1,38 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { withRouter } from 'react-router-dom';
 import { connect } from 'react-redux';
 
 import './question-preview.scss';
-import { QuestionAction } from '../../../state/actions/questions';
 
 const QuestionText = ({ text }) => (
   <div className="question-preview__text content--red">{text}</div>
 );
 
-const QuestionButton = ({ onClick: answerQuestion }) => (
-  <button className="question-preview__button content--grey" onClick={answerQuestion}>
-    <i className="material-icons">check</i>
-  </button>
-);
-
-const QuestionOption = ({ option, text, allowToAnswer, onSelected: answerQuestion }) => {
+const QuestionOption = ({ option, text }) => {
   const optionClassName = 'question-preview__option--' + (option === 1 ? 'one' : 'two');
   return (
     <section className={'question-preview__option ' + optionClassName}>
-      {allowToAnswer && <QuestionButton onClick={answerQuestion} />}
       <QuestionText text={text} />
     </section>
   );
 };
 
-class QuestionPreview extends React.Component {
+class QuestionPreview extends Component {
   navigateToQuestion = () => {
     const { question, history } = this.props;
     history.push('/question/' + question.id);
   };
 
   render() {
-    const { username, allowToAnswer, answerQuestion, question } = this.props;
+    const { allowToAnswer, question } = this.props;
     const { optionOne, optionTwo } = question;
     return (
       <article className="question-preview" onClick={this.navigateToQuestion}>
-        <QuestionOption
-          option={1}
-          text={optionOne.text}
-          allowToAnswer={allowToAnswer}
-          onSelected={() => answerQuestion(username, question.id, 'optionOne')}
-        />
-        <QuestionOption
-          option={2}
-          text={optionTwo.text}
-          allowToAnswer={allowToAnswer}
-          onSelected={() => answerQuestion(username, question.id, 'optionTwo')}
-        />
+        <QuestionOption option={1} text={optionOne.text} allowToAnswer={allowToAnswer} />
+        <QuestionOption option={2} text={optionTwo.text} allowToAnswer={allowToAnswer} />
       </article>
     );
   }
 }
 
-const stateToProps = (state, props) => ({
-  ...props,
-  username: state.auth.username
-});
-
-const dispatchToProps = dispatch => ({
-  answerQuestion: (username, questionId, option) => {
-    dispatch(QuestionAction.answerQuestion(username, questionId, option));
-  }
-});
-
-export default withRouter(
-  connect(
-    stateToProps,
-    dispatchToProps
-  )(QuestionPreview)
-);
+export default withRouter(connect()(QuestionPreview));
