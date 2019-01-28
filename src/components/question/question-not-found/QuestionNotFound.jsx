b1391d774dca65d8ede5975c02024d61a372408a
John <john.19col@gmail.com>
2019-01-28 18:44:13 -0500

    Update question not found styles

--- src/components/question/question-not-found/QuestionNotFound.jsx
+++ src/components/question/question-not-found/QuestionNotFound.jsx
@@ -1,10 +1,14 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 import './question-not-found.scss';
 
 const QuestionNotFound = () => (
   <div className="question-not-found">
     <span className="question-not-found__text">Question Not Found</span>
+    <Link className="question-not-found__link" to="/">
+      Go home
+    </Link>
   </div>
 );
 
