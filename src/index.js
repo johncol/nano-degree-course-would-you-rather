1a34e68c874b9b6122f219085a83ae6d3907115b
John Cely <john.19col@gmail.com>
2019-02-01 13:46:12 -0500

    Move the store creation logic to its own state/store.js file

--- src/index.js
+++ src/index.js
@@ -1,16 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { createStore } from 'redux';
 import { Provider } from 'react-redux';
 
 import App from './components/App';
 import Loader from './components/shared/loader/Loader';
-import reducers from './state/reducers';
-import middlewares from './state/middlewares';
+import store from './state/store';
 
 import './styles/index.scss';
-
-const store = createStore(reducers, middlewares);
 
 ReactDOM.render(
   <Provider store={store}>
